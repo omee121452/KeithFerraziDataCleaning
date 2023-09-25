237827f2c8633f36bf8fd25a902367faef5dd8fe
omee121452 <omwins2@gmail.com>
2023-09-24 22:10:50 -0700

    added comments

--- main.py
+++ main.py
@@ -3,33 +3,36 @@ from pdfminer.high_level import extract_pages, extract_text
 import PyPDF2
 import os
 
+#Store pdf data into text variable
 text = extract_text("NeverEatAlone.pdf")
 organizedText = text 
+#get current path of user
 currentPath = os.getcwd()
 
-#make all text lower case
+#cleaning text
 organizedText = organizedText.lower()
-#remove white space
 organizedText = re.sub(" ", "", organizedText)
 organizedText = re.sub('\n', '', organizedText)
-#remove Punctuation
 organizedText = re.sub(r'[^\w\s]', '', organizedText)
 testText= extract_pages(organizedText[15])
 print(testText)
 
 
 folderName="KeithFerrazzi_Data"
-
+#Create folder for user if needed
 if not os.path.exists(folderName):
     os.makedirs(folderName)
 else:
     print("KeithFerrazzi_Data folder already exists.")
 
-#Everytime you add a new file make sure you change the filename to something different so then you don't overwrite old data.
+#checking if a folder has been made in order 
 if os.path.exists(folderName):
+    #Everytime you add a new file make sure you change the filename so you don't overwrite old data.
     filename='NeverEatAlone.txt'
+    #gets the current path to include the recently made folder
     path= os.path.join(currentPath ,folderName)
-    print(path)
+    #This is the path where the file will be written into
     fileWritePath = os.path.join(path, filename)
+    #creating file
     with open(fileWritePath, 'w') as file:
         file.write(organizedText)
